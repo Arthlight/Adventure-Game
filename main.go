eed42051ed006f843e32ca503ddba2b9d80dc83a
Arthlight <arthurpbh@gmail.com>
2020-04-17 17:59:01 +0200

    added structs for the json to unmarshal

--- main.go
+++ main.go
@@ -1,17 +1,38 @@
 package main
 
 import (
+	"flag"
+	"fmt"
+	"log"
 	"html/template"
 	"encoding/json"
+	"io/ioutil"
 )
 
-type Story struct {
-	Title string
-	Story []string
-	Options MyOptions
+type Story map[string]Chapter
+
+type Chapter struct {
+	Title   string   `json:"title"`
+	Story   []string `json:"story"`
+	Options []Options `json:"options"`
 }
 
-type MyOptions struct {
-	text string
-	arc string
+type Options struct {
+	Text string `json:"text"`
+	Arc  string `json:"arc"`
+}
+
+func main() {
+	filename := flag.String("file", "adventure.json", "the file to choose for your adventure")
+	flag.Parse()
+
+	file, err := ioutil.ReadFile(*filename)
+	if err != nil {
+		log.Println(err)
+	}
+	var myStory Chapter
+	err = json.Unmarshal(file, &myStory)
+	if err != nil {
+		log.Println(err)
+	}
 }
